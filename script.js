6b26f9915652e0f87bb4ea4db5381765cdcd4bf9
Josh Mathiak <jmathiak4@gmail.com>
2022-02-22 11:24:31 -0600

    Functional w/ decimals

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
-let firstNum = "";
-let secondNum = "";
+let firstNum = "0";
+let secondNum = "0";
 let op = "";
 let result = "";
 let display = "";
 let onSecond = false;
+let inEquation = false;
+let hasDec = false;
 
 function add() {
-  const num1 = parseInt(firstNum, 10);
-  const num2 = parseInt(secondNum, 10);
+  const num1 = parseFloat(firstNum, 10);
+  const num2 = parseFloat(secondNum, 10);
   result = num1 + num2;
+  let res = result.toFixed(2);
+  result = res;
+
+  if (result % 1 === 0) {
+    let res = result;
+    result = parseInt(res);
+  }
 }
 
 function sub() {
-  const num1 = parseInt(firstNum, 10);
-  const num2 = parseInt(secondNum, 10);
+  const num1 = parseFloat(firstNum, 10);
+  const num2 = parseFloat(secondNum, 10);
   result = num1 - num2;
+  let res = result.toFixed(2);
+  result = res;
+
+  if (result % 1 === 0) {
+    let res = result;
+    result = parseInt(res);
+  }
 }
 
 function mult() {
-  const num1 = parseInt(firstNum, 10);
-  const num2 = parseInt(secondNum, 10);
+  const num1 = parseFloat(firstNum, 10);
+  const num2 = parseFloat(secondNum, 10);
   result = num1 * num2;
+  let res = result.toFixed(2);
+  result = res;
+
+  if (result % 1 === 0) {
+    let res = result;
+    result = parseInt(res);
+  }
 }
 
 function div() {
-  const num1 = parseInt(firstNum, 10);
-  const num2 = parseInt(secondNum, 10);
+  const num1 = parseFloat(firstNum, 10);
+  const num2 = parseFloat(secondNum, 10);
   result = num1 / num2;
+  let res = result.toFixed(2);
+  result = res;
+
+  if (result % 1 === 0) {
+    let res = result;
+    result = parseInt(res);
+  }
 }
 
 function operate() {
@@ -40,19 +70,43 @@ function operate() {
     div();
   }
   document.getElementById("display").innerHTML = result;
+  firstNum = result;
+  inEquation = true;
+  onSecond = false;
 }
 
 function setOperator(operator) {
   op = operator;
   onSecond = true;
+  hasDec = false;
 }
 
 function getInput(num) {
-  if (!onSecond) {
-    firstNum = firstNum + num;
-    document.getElementById("display").innerHTML = firstNum;
-  } else {
-    secondNum = secondNum + num;
-    document.getElementById("display").innerHTML = secondNum;
+  if ((!hasDec && num === ".") || num !== ".") {
+    if (!onSecond) {
+      if (firstNum === "0" && num !== ".") {
+        firstNum = "";
+      }
+      firstNum = firstNum + num;
+      document.getElementById("display").innerHTML = firstNum;
+    } else {
+      if (secondNum === "0" && num !== ".") {
+        secondNum = "";
+      }
+      secondNum = secondNum + num;
+      document.getElementById("display").innerHTML = secondNum;
+    }
   }
+  if (num === ".") {
+    hasDec = true;
+  }
+}
+
+function allClear() {
+  op = "";
+  firstNum = "0";
+  secondNum = "0";
+  document.getElementById("display").innerHTML = 0;
+  onSecond = false;
+  hasDec = false;
 }
